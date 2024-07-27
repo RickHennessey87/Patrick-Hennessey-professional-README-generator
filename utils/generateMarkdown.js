022f8acecde87886d1ce6d95bea545bf62e2d744
Patrick Hennessey <pjhennessey89@gmail.com>
2024-07-27 11:46:22 -0500

    updated the generateMarkdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,18 +13,63 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   if (license === 'None') {
     return '';
-  } else {}
+  } else {
+    return `[License](#license)`;
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  } else {
+    return `
+    ## License
+    
+    This project is covered by the ${license} license.`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+    ${renderLicenseBadge(data.license)}
+
+    ## Description
+        
+    ${data.description}
+        
+    ## Table of Contents
+        
+    - [Installation] (#installation)
+    - [Usage] (#usage)
+    - [Credits] (#credits)
+    - [License] (#license)
+        
+    ## Installation
+        
+    ${data.installation}
+        
+    ## Usage
+        
+    ${data.usage}
+        
+    ## Credits
+        
+    GitHub: ${data.github}
+    Email: ${data.email}
+        
+    ${renderLicenseSection(data.license)}
+        
+    ## How to Contribute 
+
+    ${data.contribution}
+        
+    ## Tests
+        
+    ${data.test}`;
 }
 
 module.exports = generateMarkdown;
